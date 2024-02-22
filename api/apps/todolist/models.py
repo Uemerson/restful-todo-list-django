5f8693ee18f251232cc23f9d2365e17361f61cbf
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-22 14:21:26 -0300

    feat: add todolist model

--- api/apps/todolist/models.py
+++ api/apps/todolist/models.py
@@ -1,3 +1,12 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class TodoList(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=250, blank=False, null=False)
+    description = models.CharField(max_length=250, blank=False, null=False)
+    concluded = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
