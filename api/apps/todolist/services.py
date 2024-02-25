0afbee8b6b0166adb1611a44f209f7251e5c812c
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 03:19:32 -0300

    feat: add retrieve todo list service

--- api/apps/todolist/services.py
+++ api/apps/todolist/services.py
@@ -5,13 +5,13 @@ from .serializers import TodoListSerializer
 
 
 class TodoListService:
-    def create(self, todo_list: TodoListSerializer) -> TodoListSerializer:
+    def create(self, todo_list: TodoListSerializer):
         todo_list.save()
         for key in ['list-todolist']:
             cache.delete(key)
         return todo_list
 
-    def list(self) -> list[dict]:
+    def list(self):
         todo_lists = cache.get('list-todolist')
         if not todo_lists:
             todo_lists = TodoList.objects.all()
@@ -19,3 +19,12 @@ class TodoListService:
             cache.set('list-todolist', serializer.data, timeout=None)
             return serializer.data
         return todo_lists
+
+    def retrieve(self, pk: str):
+        todo_list = cache.get(f'retrieve-todolist-{pk}')
+        if not todo_list:
+            todo_list = TodoList.objects.filter(pk=pk).first()
+            serializer = TodoListSerializer(todo_list)
+            cache.set(f'retrieve-todolist-{pk}', serializer.data, timeout=None)
+            return serializer.data
+        return todo_list
