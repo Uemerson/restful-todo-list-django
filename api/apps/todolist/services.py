3a0d18d6c353002fbe90fa838c7942acb5aaf2c2
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 00:19:38 -0300

    feat: add list todo list service

--- api/apps/todolist/services.py
+++ api/apps/todolist/services.py
@@ -1,7 +1,23 @@
+from django.core.cache import cache
+
+from .models import TodoList
 from .serializers import TodoListSerializer
 
 
 class AddTodoListService:
     def handle(self, todo_list: TodoListSerializer) -> TodoListSerializer:
         todo_list.save()
+        for key in ['list-todolist']:
+            cache.delete(key)
         return todo_list
+
+
+class ListTodoListService:
+    def handle(self) -> dict:
+        todo_lists = cache.get('list-todolist')
+        if not todo_lists:
+            todo_lists = TodoList.objects.all()
+            serializer = TodoListSerializer(todo_lists, many=True)
+            cache.set('list-todolist', serializer.data, timeout=None)
+            return serializer.data
+        return todo_lists
