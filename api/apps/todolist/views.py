220e6834d9cf0b5ea5d8b45c8aed48910f778f7b
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 03:20:06 -0300

    feat: add retrieve method to todo list viewset

--- api/apps/todolist/views.py
+++ api/apps/todolist/views.py
@@ -20,3 +20,7 @@ class TodoListViewSet(viewsets.ViewSet):
     def list(self, request):
         todo_lists = self.__todo_list_service.list()
         return Response(todo_lists, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        todo_list = self.__todo_list_service.retrieve(pk)
+        return Response(todo_list, status=status.HTTP_200_OK)
