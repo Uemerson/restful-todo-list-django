619bd4a311b215eea71a90c0ec1f5e95db69812f
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 18:59:59 -0300

    refactor: rename tests in todo list services

--- api/apps/todolist/tests/test_services.py
+++ api/apps/todolist/tests/test_services.py
@@ -17,13 +17,13 @@ from ..services import TodoListService
 )
 class TodoListServicesTestCase(TestCase):
     def setUp(self):
-        self.todolist = TodoList.objects.create(
+        self.todo_list = TodoList.objects.create(
             title='valid_title', description='valid_description'
         )
         self.todo_list_service = TodoListService()
 
     @patch('apps.todolist.services.cache.delete')
-    def test_create_todo_list_service(self, mock_delete):
+    def test_create_service(self, mock_delete):
         todo_list_data = {
             'title': 'valid_title',
             'description': 'valid_description',
@@ -37,63 +37,61 @@ class TodoListServicesTestCase(TestCase):
         )
         self.assertEqual(todo_list.data, serializer.data)
 
-    def test_list_todo_list_service(self):
+    def test_list_service(self):
         todo_lists = self.todo_list_service.list()
-        self.assertEqual(todo_lists, [TodoListSerializer(self.todolist).data])
+        self.assertEqual(todo_lists, [TodoListSerializer(self.todo_list).data])
 
     @patch('apps.todolist.services.cache.get')
-    def test_should_call_get_cache_in_list_todo_list_service(self, mock_get):
+    def test_should_call_get_cache_in_list_service(self, mock_get):
         self.todo_list_service.list()
         mock_get.assert_called_once()
 
     @patch('apps.todolist.services.cache.set')
-    def test_should_call_set_cache_in_list_todo_list_service(self, mock_set):
+    def test_should_call_set_cache_in_list_service(self, mock_set):
         todo_lists = self.todo_list_service.list()
         mock_set.assert_called_once()
-        self.assertEqual(todo_lists, [TodoListSerializer(self.todolist).data])
+        self.assertEqual(todo_lists, [TodoListSerializer(self.todo_list).data])
 
-    def test_retrieve_todo_list_service(self):
-        todo_list = self.todo_list_service.retrieve(self.todolist.pk)
-        self.assertEqual(todo_list, TodoListSerializer(self.todolist).data)
+    def test_retrieve_service(self):
+        todo_list = self.todo_list_service.retrieve(self.todo_list.pk)
+        self.assertEqual(todo_list, TodoListSerializer(self.todo_list).data)
 
     @patch('apps.todolist.services.cache.get')
-    def test_should_call_get_cache_in_retrieve_todo_list_service(
-        self, mock_get
-    ):
-        self.todo_list_service.retrieve(self.todolist.pk)
+    def test_should_call_get_cache_in_retrieve_service(self, mock_get):
+        self.todo_list_service.retrieve(self.todo_list.pk)
         mock_get.assert_called_once()
 
     @patch('apps.todolist.services.cache.set')
-    def test_should_call_set_cache_in_retrieve_todo_list_service(
-        self, mock_set
-    ):
-        todo_list = self.todo_list_service.retrieve(self.todolist.pk)
+    def test_should_call_set_cache_in_retrieve_service(self, mock_set):
+        todo_list = self.todo_list_service.retrieve(self.todo_list.pk)
         mock_set.assert_called_once()
-        self.assertEqual(todo_list, TodoListSerializer(self.todolist).data)
+        self.assertEqual(todo_list, TodoListSerializer(self.todo_list).data)
 
-    def test_should_raise_404_when_id_not_found_in_retrieve_todo_list_service(
+    def test_should_raise_404_when_id_not_found_in_retrieve_service(
         self,
     ):
         with self.assertRaises(Http404):
-            self.todolist.delete()
-            self.todo_list_service.retrieve(self.todolist.pk)
+            self.todo_list.delete()
+            self.todo_list_service.retrieve(self.todo_list.pk)
 
     @patch('apps.todolist.services.cache.delete')
-    def test_destroy_todo_list_service(self, mock_delete):
-        self.todo_list_service.destroy(self.todolist.pk)
+    def test_destroy_service(self, mock_delete):
+        self.todo_list_service.destroy(self.todo_list.pk)
         self.assertEqual(mock_delete.call_count, 2)
         self.assertTrue(
             mock_delete.call_args_list,
             [
                 (('list-todolist')),
-                ((f'retrieve-todolist-{self.todolist.pk}')),
+                ((f'retrieve-todolist-{self.todo_list.pk}')),
             ],
         )
-        self.assertFalse(TodoList.objects.filter(id=self.todolist.pk).exists())
+        self.assertFalse(
+            TodoList.objects.filter(id=self.todo_list.pk).exists()
+        )
 
-    def test_should_raise_404_when_id_not_found_in_destroy_todo_list_service(
+    def test_should_raise_404_when_id_not_found_in_destroy_service(
         self,
     ):
         with self.assertRaises(Http404):
-            self.todolist.delete()
-            self.todo_list_service.destroy(self.todolist.pk)
+            self.todo_list.delete()
+            self.todo_list_service.destroy(self.todo_list.pk)
