6551dca9c675d363c44f5648ebdf1fd39b01de93
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 02:17:41 -0300

    test: test: list todolist viewset

--- api/apps/todolist/tests/test_view.py
+++ api/apps/todolist/tests/test_view.py
@@ -1,14 +1,35 @@
-from django.test import TestCase
+import json
+
+from django.test import TestCase, override_settings
 from django.urls import reverse
 from rest_framework import status
 
 from ..models import TodoList
+from ..serializers import TodoListSerializer
 
 
+@override_settings(
+    CACHES={
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
+)
 class TodoListViewsTestCase(TestCase):
+    def setUp(self):
+        self.todolist = TodoList.objects.create(
+            title='valid_title', description='valid_description'
+        )
+
     def test_create_todo_list_viewset(self):
         data = {'title': 'valid_title', 'description': 'valid_description'}
         count_todo_list = TodoList.objects.count()
-        response = self.client.post(reverse('create-todolist'), data=data)
+        response = self.client.post(reverse('todolist'), data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(TodoList.objects.count(), count_todo_list + 1)
+
+    def test_list_todo_list_viewset(self):
+        response = self.client.get(reverse('todolist'))
+        content = json.loads(response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(content, [TodoListSerializer(self.todolist).data])
