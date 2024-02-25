2bced529e9ce416ca768bb42b00ed702506e5e5c
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2024-02-25 01:59:24 -0300

    test: add test service list todo list

--- api/apps/todolist/tests/test_services.py
+++ api/apps/todolist/tests/test_services.py
@@ -1,12 +1,27 @@
-from django.test import TestCase
+from unittest.mock import patch
+
+from django.test import TestCase, override_settings
 
 from ..models import TodoList
 from ..serializers import TodoListSerializer
-from ..services import AddTodoListService
+from ..services import AddTodoListService, ListTodoListService
 
 
+@override_settings(
+    CACHES={
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
+)
 class TodoListServicesTestCase(TestCase):
-    def test_add_todo_list_service(self):
+    def setUp(self):
+        self.todolist = TodoList.objects.create(
+            title='valid_title', description='valid_description'
+        )
+
+    @patch('apps.todolist.services.cache.delete')
+    def test_add_todo_list_service(self, mock_delete):
         todo_list_data = {
             'title': 'valid_title',
             'description': 'valid_description',
@@ -15,6 +30,26 @@ class TodoListServicesTestCase(TestCase):
         self.assertTrue(serializer.is_valid())
         service = AddTodoListService()
         saved_todo_list = service.handle(serializer)
-
-        self.assertTrue(TodoList.objects.filter(title='valid_title').exists())
+        mock_delete.assert_called_once()
+        self.assertTrue(
+            TodoList.objects.filter(id=saved_todo_list.data['id']).exists()
+        )
         self.assertEqual(saved_todo_list.data, serializer.data)
+
+    def test_list_todo_list_service(self):
+        service = ListTodoListService()
+        todo_lists = service.handle()
+        self.assertEqual(todo_lists, [TodoListSerializer(self.todolist).data])
+
+    @patch('apps.todolist.services.cache.get')
+    def test_should_call_get_cache_in_list_todo_list_service(self, mock_get):
+        service = ListTodoListService()
+        service.handle()
+        mock_get.assert_called_once()
+
+    @patch('apps.todolist.services.cache.set')
+    def test_should_call_set_cache_in_list_todo_list_service(self, mock_set):
+        service = ListTodoListService()
+        todo_lists = service.handle()
+        mock_set.assert_called_once()
+        self.assertEqual(todo_lists, [TodoListSerializer(self.todolist).data])
